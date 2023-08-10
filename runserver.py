4b277678437546e5a90c3ad4d4c0b7f9f740c4dc
Jojo <fjdiaz2k11@hotmail.com>
2023-08-10 13:51:13 -0500

    add upload template with code changes

--- runserver.py
+++ runserver.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 from os import environ
 from db import queries as que
 from neoloadreporter import xmlreporter as x
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'xml'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello_world():
-    #que.basequery('testing')
-    #x.neoload_xml_reader('testing','C:\\Users\\Cisco\\Documents\\Python Projects\\webappdashboard\\reporterWebApp\\reportsample.xml')
-    return "<p> Hello, World</p>"
+     return "<p> Hello, World</p>"
 
 @app.route('/teamportal/<team>', methods =['GET', 'POST'])
 def teampage(team):
     data = que.basequery(team)
     return render_template('teamportal.html', team_name=team, testrun=data)
-
 
 @app.route('/template1', methods=['GET'])
 def sample1():
@@ -25,9 +27,25 @@ def sample1():
 
 @app.route('/upload', methods=['GET','POST'])
 def uploadfile():
-    x.neoload_xml_reader('testing','C:\\Users\\Cisco\\Documents\\Python Projects\\webappdashboard\\reporterWebApp\\reportsample.xml')
-    return render_template('upload.html')
+     if request.method == 'POST':
+        file = request.files['files']
+        if file.filename == '':
+            flash('No selected file')
+        if file and allowed_files(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(environ.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+        # team_selection = request.form['team']
+        # environment = request.form['env']
+        # report_query_location = request.form['uploadtype']
+        # x.neoload_xml_reader(team_selection,report_query_location)
+        return "<h1> Data has been uploaded</h1>"
+     else:
+         return render_template('upload.html')
 
+def allowed_files(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 
