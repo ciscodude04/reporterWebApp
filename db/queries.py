57480bc0682770d38433f937bcf9c613dc1a9fed
Francisco J <fjdiaz2k11@hotmail.com>
2023-08-24 15:49:43 -0500

    Updated the upload method

--- db/queries.py
+++ db/queries.py
@@ -8,7 +8,7 @@ def basequery(team):
     connection = dbconn.sqlite_open_connection(mydb.SQLiteConnect.stagedb)
     df = pd.read_sql_query(f'''
                            select DISTINCT test_run_name, count,min, average,max, count, percentile_95, std_deviation FROM {team} order by end_time desc LIMIT 10;''', connection)
-    connection.close()
+    dbconn.sqlite_connection_close(connection)
     return df
 
 def base_reports(team):
